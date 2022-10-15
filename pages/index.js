1269b62a690e8da2ff3bad523f05d8c85e8cbedc
Zach Lankton <zachlankton@gmail.com>
2022-10-15 09:07:16 -0400

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -19,6 +20,7 @@ export default function Home() {
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
+          <button><Link href="/test">Goto test page</Link></button>
         </p>
 
         <div className={styles.grid}>
